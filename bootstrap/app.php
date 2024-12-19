619e3b2c3cf02108160df101c254d84ae847217d
Bucky <bucky@stxcables.com>
2024-12-19 11:18:25 -0600

    Connectwise PSA webhook test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Middleware\IntegrationMiddleware;
 use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
@@ -14,7 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->alias([
-            'role' => RoleMiddleware::class
+            'role' => RoleMiddleware::class,
+            'integration' => IntegrationMiddleware::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
