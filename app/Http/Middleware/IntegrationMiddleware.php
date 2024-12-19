d891d67d1eec1a94c3d1fac3a179770de8ea2f7d
Bucky <bucky@stxcables.com>
2024-12-19 12:09:15 -0600

    Connectwise PSA webhook test

--- app/Http/Middleware/IntegrationMiddleware.php
+++ app/Http/Middleware/IntegrationMiddleware.php
@@ -16,8 +16,15 @@ class IntegrationMiddleware
     public function handle(Request $request, Closure $next, string $type): Response
     {
         if ($type == 'connect-wise') {
+
+            $hashedKey = hash('sha256', config('cw.private_key'), true);
+
+            // Compute HMAC-SHA256 using the hashed key
+            $hmac = hash_hmac('sha256', $request->get('Entity'), $hashedKey, true);
+
             $request->merge([
-                'Entity' => json_decode($request->get('Entity'), true)
+                'Entity' => json_decode($request->get('Entity'), true),
+                'Hash' => base64_encode($hmac)
             ]);
         }
 
