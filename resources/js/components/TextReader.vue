dacf2b6bcdebfb33c51c052f81af7b7cf69a50ca
Bucky <bucky@stxcables.com>
2025-01-02 10:35:19 -0600

    Recounting report

--- resources/js/components/TextReader.vue
+++ resources/js/components/TextReader.vue
@@ -4,7 +4,7 @@
             <div class="position-relative" id="txtScanner">
                 <camera
                     @snapshot="snapshop"
-                    :resolution="{ width: 3000, height: 3000 }"
+                    :resolution="{ width: 2000, height: 2000 }"
                 />
             </div>
         </div>
