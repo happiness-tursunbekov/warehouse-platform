b1d5257394306d76ad66b1612c4df8d6ea98ef9c
Bucky <bucky@stxcables.com>
2025-04-03 10:54:29 -0500

    BigCommerce channel asignment is automated

--- app/Http/Controllers/Integration/BigCommerceController.php
+++ app/Http/Controllers/Integration/BigCommerceController.php
@@ -3,14 +3,16 @@
 namespace App\Http\Controllers\Integration;
 
 use App\Http\Controllers\Controller;
+use App\Models\Setting;
 use App\Models\WebhookLog;
 use App\Services\BigCommerceService;
+use App\Services\ConnectWiseService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 
 class BigCommerceController extends Controller
 {
-    public function productCreated(Request $request, BigCommerceService $bigCommerceService)
+    public function productCreated(Request $request, BigCommerceService $bigCommerceService, ConnectWiseService $connectWiseService)
     {
         $data = $request->get('data');
 
@@ -24,6 +26,13 @@ class BigCommerceController extends Controller
                 $bigCommerceService->createProductModifier($product->id, BigCommerceService::PRODUCT_OPTION_COMPANY, sort_order: 3);
                 $bigCommerceService->createProductModifier($product->id, BigCommerceService::PRODUCT_OPTION_SERVICE_TICKET, sort_order: 4);
                 $bigCommerceService->createProductModifier($product->id, BigCommerceService::PRODUCT_OPTION_BUNDLE, sort_order: 5);
+
+                $catalogItem = $connectWiseService->getCatalogItemByIdentifier($product->sku);
+
+                if ($catalogItem) {
+                    $bigCommerceService->setProductChannels($product->id, [1]);
+                    $bigCommerceService->setProductCategories($product->id, [332]);
+                }
             } catch (\Exception) {}
         }
     }
