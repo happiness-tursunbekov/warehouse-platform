9912a6b8b7c270315bd2ddccbabdafd92f51918e
Bucky <bucky@stxcables.com>
2025-04-21 09:08:27 -0500

    companies service updated

--- app/Http/Controllers/Integration/ConnectWiseController.php
+++ app/Http/Controllers/Integration/ConnectWiseController.php
@@ -396,7 +396,7 @@ class ConnectWiseController extends Controller
 
     public function companies(ConnectWiseService $connectWiseService)
     {
-        return response()->json($connectWiseService->getCompanies(1, 'deletedFlag=false', null, 'id,name,company'));
+        return response()->json($connectWiseService->getCompanies(1, null, 'id,name,company'));
     }
 
     public function bundles(Request $request, ConnectWiseService $connectWiseService)
