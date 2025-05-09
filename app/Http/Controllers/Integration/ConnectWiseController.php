df1736f36befc60db7fb92c2ef250a865ed2cf7e
Bucky <bucky@stxcables.com>
2025-05-09 18:33:47 -0500

    fixes

--- app/Http/Controllers/Integration/ConnectWiseController.php
+++ app/Http/Controllers/Integration/ConnectWiseController.php
@@ -207,8 +207,6 @@ class ConnectWiseController extends Controller
 
             case ConnectWiseService::ACTION_UPDATED:
 
-                sleep(2);
-
                 $poItems = collect($connectWiseService->purchaseOrderItemsOriginal($po->id))
                     ->filter(function ($poItem) use (&$po) {
 
